185dfbfbf05ee978e0a323331786ed789afd6067
James Spencer <jameskcs1254@gmail.com>
2021-06-18 13:27:46 +0100

    Add pseudocode skeleton for functions creating the grid

---
+++ etch.js
@@ -0,0 +1,25 @@
+//function: gridCreate (n)
+function gridCreate(n) {
+    //create a div variable
+    const div = document.createElement('div');
+    //create variable containg sketchbox
+    const sketchBox = document.querySelector('.sketch-box');
+
+        
+    //add 'box' class to div#
+    div.classList.add('box')
+    //execute cssChange(n) 
+
+    //For loop up to n**2 
+        //append div variable to container variable
+
+
+
+}
+
+
+//func: ccsChange(n) 
+    // create percentString variable = (1/n *100) + "% "
+    //create variable cssString = string literal of (percentString * n)
+
+    //change css class "box" to contain value of cssString for grid-template-columns (style.gridTemplateColumns = "50px 50px 50px")
