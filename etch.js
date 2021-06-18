eae90615c871f14e05636b4e34f22ef6183542cf
James Spencer <jameskcs1254@gmail.com>
2021-06-18 13:39:13 +0100

    first attempt filling in pseudocode with JS

--- etch.js
+++ etch.js
@@ -5,21 +5,26 @@ function gridCreate(n) {
     //create variable containg sketchbox
     const sketchBox = document.querySelector('.sketch-box');
 
-        
     //add 'box' class to div#
-    div.classList.add('box')
+    div.classList.add('box');
     //execute cssChange(n) 
+    cssChange(n, sketchBox);
 
     //For loop up to n**2 
+    for (let i=0; i>(n**2); i++){
         //append div variable to container variable
-
-
-
+        sketchBox.appendChild(div);
+    }
 }
 
 
 //func: ccsChange(n) 
+function cssChange(n, sketchBox) {
     // create percentString variable = (1/n *100) + "% "
+    let percentString = ((1/n)*100) + '% ';
     //create variable cssString = string literal of (percentString * n)
+    let cssString = `${percentString * n}`;
 
     //change css class "box" to contain value of cssString for grid-template-columns (style.gridTemplateColumns = "50px 50px 50px")
+    sketchBox.style.gridTemplateColumns = cssString;
+}
